a3b503b07ae1e99c7db33490f1543aee6ae5d537
DorcasNjoku <dollybrownchinyere@gmail.com>
2022-12-12 18:12:03 -0800

    The success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 /**
  * main - prints all combinations of three digits with,
  * and space followed by new line
@@ -7,29 +7,27 @@
  */
 int main(void)
 {
-        int hund;
-        int tens;
-        int ones;
-
-        for (hund = 0; hund <= 9; hund++)
-        {
-                for (tens = hund + 1; tens <= 9; tens++)
-                {
-                        for (ones = tens + 1; ones <= 9; ones++)
-                        {
-                                putchar(hund + '0');
-                                putchar(tens + '0');
-                                putchar(ones + '0');
-
-                                if (hund < 7)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+int hund;
+int tens;
+int ones;
+for (hund = 0; hund <= 9; hund++)
+{
+for (tens = hund + 1; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(hund + '0');
+putchar(tens + '0');
+putchar(ones + '0');
+if (hund < 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
 
