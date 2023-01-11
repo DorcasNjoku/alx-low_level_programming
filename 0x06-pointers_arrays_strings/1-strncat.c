334479c1332df5d6170419a8d6b9f8d17b13b389
Dorcas Njoku <114452606+DorcasNjoku@users.noreply.github.com>
2023-01-11 00:28:29 +0000

    Add files via upload

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates two strings.
+ * _strncat - function that concatenates two strings.
  * @dest: the pointer
- * @src: the pointer
+ * @src: the character
+ * @n: integer
  * Return: success
  */
-char *_strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
